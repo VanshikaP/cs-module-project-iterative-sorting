9e7f755419b0c47af0eb8ffee671fe77e9ab109d
VanshikaP <vanshikapundir92@gmail.com>
2020-07-20 12:10:15 -0700

    searching algorithms

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if target is arr[i]:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,23 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    if len(arr) is 0:
+        return -1
+    if arr[0] is target:
+        return 0
+    
+    startIndex = 0
+    endIndex = len(arr) - 1
 
+    while startIndex < endIndex:
+        middleIndex = int((endIndex - startIndex) / 2)
+        if target is arr[middleIndex]:
+            return middleIndex
+        elif target < arr[middleIndex]:
+            endIndex = middleIndex - 1
+        else:
+            startIndex = middleIndex + 1
 
+    if arr[startIndex] is target:
+        return startIndex
     return -1  # not found
